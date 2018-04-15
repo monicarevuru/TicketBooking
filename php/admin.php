3d2bdffa871ad5f12090f7c32f99c38713c52d14
himavarshini <himayarragangu@gmail.com>
2018-04-15 23:41:41 +0530

    Admin login backend

--- php/admin.php
+++ php/admin.php
@@ -7,8 +7,12 @@ if ( $conn ) {
 $email = $_GET['email'];
 $password = $_GET['password'];
 $sql = "SELECT * FROM `BookTickz`.`admin` WHERE `admin_email` = '$email' and `admin_password` = '$password'";
+$result = mysql_query( $sql);
 //echo $sql;
-if ( mysql_query($sql) ) {
+if ( mysql_num_rows( $result ) > 0 ) {
   echo "Admin logged in";
+}
+else{
+  echo "Invalid admin login";
 }
 ?>
