9c8315c690ea64b140a3b831015dfe138063d83c
sarathchandrika <sarath182015@gmail.com>
2018-04-22 12:27:11 +0000

    Front-end validation for RegisterPage

--- php/recieve.php
+++ php/recieve.php
@@ -4,31 +4,37 @@ include("config.php");
 $email = $_GET['email'];
 $password = $_GET['password'];
 $name = $_GET['name'];
+$cnfrm = $_GET['cnfrmpass'];
 
 $emaillen = strlen( $email );
 $i=$emaillen;
 $char = substr( $email, $emaillen-10, 10 );
 //echo $char;
-if(strcmp($char,'@gmail.com')!=0||strcmp($char,'@yahoo.com')!=0||strcmp($char,'@outlook.com')!=0){
+if(strcmp($char,'@gmail.com')!=0&&strcmp($char,'@yahoo.com')!=0&&strcmp($char,'@outlook.com')!=0){
   echo "Invalid mail";
 }
 else{
-if ( $conn ) {
-	//echo "connection succesfull";
-}
-/*if ( mysql_select_db($mysqldb) ) {
-	echo "Selected db succesfully";
-}*/
-$sql = "INSERT INTO user(user_name, user_email , user_password) VALUES ('$name','$email','$password')";
-//echo $sql;
+  if(strcmp($password,$cnfrm)!=0){
+    echo "Passwords didn't match";
+  }
+  else{
+    if ( $conn ) {
+    	//echo "connection succesfull";
+      $sql = "INSERT INTO `BookTickz`.`user`(`user_name`, `user_email` , `user_password`) VALUES ('$name','$email','$password')";
+      //echo $sql;
+        if ( mysqli_query( $conn, $sql ) ) {
+          echo "success";
 
-if ( mysqli_query( $conn, $sql ) ) {
-  echo "success";
-  /*echo "Yay! Inserted";*/
-}
-else {
+        /*echo "Yay! Inserted";*/
+        }
+        else {
 
-  echo "invalid";
-}
+          echo "invalid";
+        }
+    }
+    /*if ( mysql_select_db($mysqldb) ) {
+    	echo "Selected db succesfully";
+    }*/
+  }
 }
 ?>
