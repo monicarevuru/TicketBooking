1b85669b5ac2f766bffd794aca486e42fdd7ba63
sarathchandrika <sarath182015@gmail.com>
2018-04-22 11:46:53 +0000

    mail constraints are added

--- php/recieve.php
+++ php/recieve.php
@@ -4,17 +4,25 @@ include("config.php");
 $email = $_GET['email'];
 $password = $_GET['password'];
 $name = $_GET['name'];
-$username = $_GET['username'];
 
+$emaillen = strlen( $email );
+$i=$emaillen;
+$char = substr( $email, $emaillen-10, 10 );
+//echo $char;
+if(strcmp($char,'@gmail.com')!=0||strcmp($char,'@yahoo.com')!=0||strcmp($char,'@outlook.com')!=0){
+  echo "Invalid mail";
+}
+else{
 if ( $conn ) {
-	/*echo "connection succesfull";*/
+	//echo "connection succesfull";
 }
-if ( mysql_select_db($mysqldb) ) {
-	/*echo "Selected db succesfully";*/
-}
-$sql = "INSERT INTO `BookTickz`.`user`(`user_email` , `user_password`,`user_name`,`user_username`) VALUES ('$email','$password','$name','$username')";
-/*echo $sql;*/
-if ( mysql_query( $sql ) ) {
+/*if ( mysql_select_db($mysqldb) ) {
+	echo "Selected db succesfully";
+}*/
+$sql = "INSERT INTO user(user_name, user_email , user_password) VALUES ('$name','$email','$password')";
+//echo $sql;
+
+if ( mysqli_query( $conn, $sql ) ) {
   echo "success";
   /*echo "Yay! Inserted";*/
 }
@@ -22,3 +30,5 @@ else {
 
   echo "invalid";
 }
+}
+?>
