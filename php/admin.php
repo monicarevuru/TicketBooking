3f1a50c2fb10ec9dd05d8433616fcd77d3832428
sarathchandrika <sarath182015@gmail.com>
2018-04-22 18:24:15 +0000

    admin database connection and page redirect to addmovie.html

--- php/admin.php
+++ php/admin.php
@@ -7,11 +7,11 @@ if ( $conn ) {
 $email = $_GET['email'];
 $password = $_GET['password'];
 $sql = "SELECT * FROM `BookTickz`.`admin` WHERE `admin_email` = '$email' and `admin_password` = '$password'";
-$result = mysql_query( $sql);
+$result = mysqli_query($conn, $sql);
 //$row = mysql_fetch_assoc($result);
 //echo $row;
-if ( mysql_num_rows( $result ) > 0 ) {
-  header("Location:/TicketBooking/addmovie.html");
+if ( mysqli_num_rows( $result ) > 0 ) {
+  header("Location:/monicarevuru-TicketBooking/addmovie.html");
 //  header("Location:addmovie.html");
 }
 else{
