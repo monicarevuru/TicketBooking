b990ff0e63114c7d3d5f80c631cc4ac4fc88f76a
himavarshini <himayarragangu@gmail.com>
2018-05-11 12:31:16 +0000

    User Wallet

--- php/wallet.php
+++ php/wallet.php
@@ -5,13 +5,15 @@ $username = $_SESSION['login_user'];
 $amount = '1000';
 mysqli_select_db( $conn, $mysqldb);
 if ( $conn ) {
-  $sql = "UPDATE `BookTickz`.`user` set `wallet_amount`=`wallet_amount` - $amount where `user_name`= $username";
+  $sql = "UPDATE `BookTickz`.`user` set `wallet_amount`=`wallet_amount` - $amount where `user_name`= '$username'";
   $result = mysqli_query( $conn, $sql);
-echo mysqli_num_rows( $result );
+  if($result == false){
+    printf("error: %s\n", mysqli_error($conn));
+  }
 
-  $s ="SELECT * from `BookTickz`.`user` where `user_name`= $username";
+  $s ="SELECT * from `BookTickz`.`user` where `user_name`= '$username'";
+$result = mysqli_query( $conn, $s);
 
-$row = mysqli_fetch_assoc($result);
 echo "rdsuccess";
 
 
