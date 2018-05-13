db8084a172516410bc70b1f3b8aa0092f875d83a
himavarshini <himayarragangu@gmail.com>
2018-05-13 11:59:10 +0530

    Adding a movie

--- php/upload.php
+++ php/upload.php
@@ -1,7 +1,8 @@
 <?php
 $moviename=$_POST['mname'];
+$description=$_POST['desc'];
 
-if(isset($_POST["submit"])){
+//if(isset($_POST['upload'])){
     $check = getimagesize($_FILES["image"]["tmp_name"]);
     if($check !== false){
         $image = $_FILES['image']['tmp_name'];
@@ -15,29 +16,61 @@ if(isset($_POST["submit"])){
         $dbName     = 'BookTickz';
 
 
-        $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+            $db = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
         // Check connection
        if($db->connect_error){
             die("Connection failed: " . $db->connect_error);
+
         }
+
+
+
+
+
+
                 $dataTime = date("Y-m-d H:i:s");
+                // $checkbox1 = $_POST['timeslot'];
+                // $checkbox2 = $_POST['timeslot2'];
+                // $checkbox3 = $_POST['timeslot3'];
+                // $checkbox4 = $_POST['timeslot4'];
+                 $timeslot = $_POST['timeslot'];
+                 if(!empty($_POST['timeslot'])) {
+
+                    $insert = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '".$timeslot[0]."')");
+                    if($timeslot[1]=='timeslot2')
+                    $insert2 = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '".$timeslot[1]."')");
+                    //$insert3 = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '$checkbox3')");
+                    if($timeslot[2]=='timeslot3')
+                    $insert3 = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '".$timeslot[2]."')");
+                    if($timeslot[3]=='timeslot4')
+                    $insert4 = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '".$timeslot[3]."')");
+}
+
+                else{
+                  echo '<script type="text/javascript">';
+                    echo 'alert("Please select atleast one timeslot.");';
+                    echo 'location.href="/TicketBooking/upload.html"';
+                    echo '</script>';
+                  }
+
+                  //  $insert4 = mysqli_query($db,"INSERT into `BookTickz`.`image`(`name`, `image`, `created`, `Description`, `Timeslot`) VALUES ('$moviename', '$imgContent', '$dataTime', '$description', '$checkbox4')");
 
        //Insert image content into database
-       $insert = $db->query("INSERT into BookTickz.image(name, image, created) VALUES ('$moviename', '$imgContent', '$dataTime')");
-       if($insert){
+
+       if($insert1 |$insert2 |$insert3 |$insert4 ){
 	   	echo '<script type="text/javascript">';
-    		echo 'alert("File uploaded successfully.");';
-    		echo 'location.href="/TicketBooking/upload.html"';
-    		echo '</script>';	
-       }else{
-		echo '<script type="text/javascript">';
-    		echo 'alert("File upload failed, please try again.");';
+    		echo 'alert("Movie uploaded successfully.");';
     		echo 'location.href="/TicketBooking/upload.html"';
     		echo '</script>';
-       }
-   }else{
-       echo "Please select an image file to upload.";
-   }
+       }else{
+		echo '<script type="text/javascript">';
+    	echo 'alert("Movie upload failed, please try again.");';
+    echo 'location.href="/TicketBooking/upload.html"';
+    echo '</script>';
+     }
+   //}//else{
+    //   echo "Please select an image file to upload.";
+   //}
 }
 ?>
